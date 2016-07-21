0d893953b9c8b18327583a1db30464100f0c90b6
Adam Buczynski <adambuczynski@gmail.com>
2016-07-22 06:04:51 +1200

    Use babel preset without global strict

--- release/meanie-angular-duplicate-requests-filter.js
+++ release/meanie-angular-duplicate-requests-filter.js
@@ -4,8 +4,6 @@
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
  * License: MIT
  */
-'use strict';
-
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 (function (window, angular, undefined) {
