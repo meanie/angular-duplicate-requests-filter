0624879cde6dded2c3b353039c20f34e54930292
Adam Reis <adambuczynski@gmail.com>
2016-12-09 21:08:42 +1300

    1.2.1

--- release/meanie-angular-duplicate-requests-filter.js
+++ release/meanie-angular-duplicate-requests-filter.js
@@ -1,7 +1,7 @@
 /**
  * meanie-angular-duplicate-requests-filter * https://github.com/meanie/angular-duplicate-requests-filter
  *
- * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
+ * Copyright (c) 2016 Adam Reis <adam@reis.nz>
  * License: MIT
  */
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
